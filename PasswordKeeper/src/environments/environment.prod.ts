155074f4eef71bdc3dafe8cfe18c7d5ad57bf71f
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-06-24 16:32:40 -0400

    install packages

--- PasswordKeeper/src/environments/environment.prod.ts
+++ PasswordKeeper/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcpHTKsGFAeV7ZpNm9Ohd9BkCH0EBWZDo",
+    authDomain: "wangf-password-keeper-1a16d.firebaseapp.com",
+    databaseURL: "https://wangf-password-keeper-1a16d.firebaseio.com",
+    projectId: "wangf-password-keeper-1a16d",
+    storageBucket: "wangf-password-keeper-1a16d.appspot.com",
+    messagingSenderId: "848853833854"
+  }
 };
--- PasswordKeeper/src/environments/environment.ts
+++ PasswordKeeper/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAcpHTKsGFAeV7ZpNm9Ohd9BkCH0EBWZDo",
+    authDomain: "wangf-password-keeper-1a16d.firebaseapp.com",
+    databaseURL: "https://wangf-password-keeper-1a16d.firebaseio.com",
+    projectId: "wangf-password-keeper-1a16d",
+    storageBucket: "wangf-password-keeper-1a16d.appspot.com",
+    messagingSenderId: "848853833854"
+  }
 };
